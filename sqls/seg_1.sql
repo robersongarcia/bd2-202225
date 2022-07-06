72fbf2db42de2751450b06248ad6bf5f792f3de9
Roberson Garcia <roberson.gar12@gmail.com>
2022-07-06 17:41:51 -0400

    se agregaron las consultas de seguridad

---
+++ sqls/seg_1.sql
@@ -0,0 +1,3 @@
+CREATE USER 'usr_sitio_web_27922357'@'serverweb' IDENTIFIED BY '27922357';
+GRANT SELECT ON 'bd2_202225_27922357'.* TO 'usr_sitio_web_27922357'@'serverweb';
+GRANT INSERT ON 'bd2_202225_27922357'.'Ventas' TO 'usr_sitio_web_27922357'@'serverweb';
---
+++ sqls/seg_2.sql
@@ -0,0 +1,9 @@
+GRANT EXECUTE ON FUNCTION  ALtaDemanda
+    TO 'bd2_202225_28161659'@'serverweb', /*Karen*/
+    'bd2_202225_28215217'@'serverweb', /*Diego*/
+    'bd2_202225_28205084'@'serverweb'; /*Pedro*/
+    
+GRANT EXECUTE ON PROCEDURE  AjustarPrecios
+    TO 'bd2_202225_28161659'@'serverweb', /*Karen*/
+    'bd2_202225_28215217'@'serverweb', /*Diego*/
+    'bd2_202225_28205084'@'serverweb'; /*Pedro*/
